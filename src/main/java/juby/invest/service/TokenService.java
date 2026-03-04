c8e60fbf14ad449b3711808092feb41d247a4d6e
kkangmen <pickofee@gmail.com>
2026-03-05 00:40:03 +0900

    [feat] 토큰 캐시 기능 구현(무수한 토큰 요청 방지)

--- src/main/java/juby/invest/service/TokenService.java
+++ src/main/java/juby/invest/service/TokenService.java
@@ -7,6 +7,10 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClient;
 
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 @Slf4j
 @Service
 @RequiredArgsConstructor
@@ -17,7 +21,21 @@ public class TokenService {
     @Value("${kis.app-key}") private String appKey;
     @Value("${kis.app-secret}") private String appSecret;
 
+    // accessToken 캐싱을 위함.
+    private String cachedToken;
+    private LocalDateTime tokenExpirationTime;
+
     public TokenDto.TokenResponse getAccessToken(){
+
+        // 캐시된 accessToken이 존재하고 accessToken 만료 시간 전일 경우
+        if (cachedToken != null && tokenExpirationTime != null && LocalDateTime.now().isBefore(tokenExpirationTime)){
+            log.info("캐시된 토큰으로 조회");
+            int seconds = (int)Duration.between(LocalDateTime.now(), tokenExpirationTime).getSeconds();
+            return new TokenDto.TokenResponse(cachedToken, seconds);
+        }
+
+        // 캐시된 accessToken이 존재하지 않을 경우
+        log.info("새로운 토큰을 발급받음");
         TokenDto.TokenRequest request = new TokenDto.TokenRequest("client_credentials", appKey, appSecret);
 
         TokenDto.TokenResponse response = investRestClient.post()
@@ -27,9 +45,12 @@ public class TokenService {
                 .body(TokenDto.TokenResponse.class);
 
         if (response == null || response.accessToken() == null){
+            log.info("토큰 발급에 실패했습니다.");
             throw new RuntimeException("토큰 발급에 실패했습니다.");
         }
 
+        this.cachedToken = response.accessToken();
+        this.tokenExpirationTime = LocalDateTime.now().plusSeconds(response.expiresIn() - 600);
         return response;
     }
 }
