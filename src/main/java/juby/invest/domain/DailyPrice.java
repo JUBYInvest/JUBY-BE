d792a4d248927257abf21fbdff814cf0d95853b2
kkangmen <pickofee@gmail.com>
2026-03-13 00:33:46 +0900

    [feature] 주식(종목코드, 종목명) 가격(날짜, OLHCV) 엔티티 구현

---
+++ src/main/java/juby/invest/domain/DailyPrice.java
@@ -0,0 +1,52 @@
+package juby.invest.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "daily_price")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class DailyPrice {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "stock_code", nullable = false)
+    private Stock stock;
+
+    @Column(name = "date", nullable = false)
+    private LocalDate date;
+
+    @Column(name = "open_price", nullable = false)
+    private Integer openPrice;
+
+    @Column(name = "high_price", nullable = false)
+    private Integer highPrice;
+
+    @Column(name = "low_price", nullable = false)
+    private Integer lowPrice;
+
+    @Column(name = "close_price", nullable = false)
+    private Integer closePrice;
+
+    @Column(name = "volume", nullable = false)
+    private Integer volume;
+
+    @Builder
+    public DailyPrice(Stock stock, LocalDate date, Integer openPrice, Integer highPrice, Integer lowPrice, Integer closePrice, Integer volume) {
+        this.stock = stock;
+        this.date = date;
+        this.openPrice = openPrice;
+        this.highPrice = highPrice;
+        this.lowPrice = lowPrice;
+        this.closePrice = closePrice;
+        this.volume = volume;
+    }
+}
