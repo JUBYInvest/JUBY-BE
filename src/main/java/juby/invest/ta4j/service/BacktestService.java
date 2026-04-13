d01c170be0dbbde30305a8adcd662f50946ea99d
kkangmen <pickofee@gmail.com>
2026-04-13 17:49:20 +0900

    Feat: 백테스트 전체 로직 구현

--- src/main/java/juby/invest/ta4j/service/BacktestService.java
+++ src/main/java/juby/invest/ta4j/service/BacktestService.java
@@ -3,12 +3,21 @@ package juby.invest.ta4j.service;
 import jakarta.transaction.Transactional;
 import juby.invest.domain.DailyPrice;
 import juby.invest.repository.DailyPriceRepository;
+import juby.invest.ta4j.converter.AnalysisCriterionConverter;
 import juby.invest.ta4j.converter.BarSeriesConverter;
+import juby.invest.ta4j.dto.BacktestResponseDto;
+import juby.invest.ta4j.strategy.BacktestStrategy;
+import juby.invest.ta4j.strategy.SmaStrategy;
+import juby.invest.ta4j.strategy.StrategyFactory;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.ta4j.core.BarSeries;
+import org.ta4j.core.Strategy;
+import org.ta4j.core.TradingRecord;
+import org.ta4j.core.backtest.BarSeriesManager;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 @Service
@@ -18,14 +27,40 @@ public class BacktestService {
 
     private final BarSeriesConverter barSeriesConverter;
     private final DailyPriceRepository dailyPriceRepository;
+    private final StrategyFactory strategyFactory;
+    private final AnalysisCriterionConverter analysisCriterionConverter;
 
+    /***
+     * 함수: 종목코드와 전략 이름을 바탕으로 전략을 실행한다.
+     * @param stockCode 종목 코드 (ex 005930)
+     */
     @Transactional
-    public BarSeries runStrategy(String stockCode){
+    public BacktestResponseDto runStrategy(String stockCode, int strategyNum){
 
+        // List<DailyPrice -> ta4j의 Barseries 변환
         List<DailyPrice> dailyPriceList = dailyPriceRepository.findByStockStockCodeOrderByDateAsc(stockCode);
         BarSeries series = barSeriesConverter.convert(dailyPriceList, stockCode);
 
-        log.info("변환된 Bar 개수: {}", series.getBarCount());
-        return series;
+        // 전략 구축 및 실행
+        BacktestStrategy strategyFound = strategyFactory.getStrategy(strategyNum); // 인터페이스 반환
+        Strategy strategy = strategyFound.strategy(series); // 실제 구현체 반환 (ex SMA)
+        BarSeriesManager manager = new BarSeriesManager(series);
+        TradingRecord record = manager.run(strategy);
+
+        System.out.println("체결된 포지션: " + record.getPositionCount());
+
+        // 결과 지표 분석
+        List<BigDecimal> analysisList = analysisCriterionConverter.converter(series, record);
+
+        return BacktestResponseDto.builder()
+                .stockCode(stockCode)
+                .strategyName(strategy.getName())
+                .totalReturn(analysisList.get(0))
+                .annualizedReturn(analysisList.get(1))
+                .positionCount(record.getPositionCount())
+                .sharpeRatio(analysisList.get(2))
+                .stdDeviation(analysisList.get(3))
+                .maxDrawdown(analysisList.get(4))
+                .build();
     }
 }
