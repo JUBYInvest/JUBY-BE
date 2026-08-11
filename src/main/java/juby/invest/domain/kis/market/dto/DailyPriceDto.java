7eb172f71c16ac8d54fdf60c999eb184057e87b6
kkangmen <pickofee@gmail.com>
2026-08-11 14:37:41 +0900

    refactor: 전일대비 변동률 응답필드 추가

--- src/main/java/juby/invest/domain/kis/market/dto/DailyPriceDto.java
+++ src/main/java/juby/invest/domain/kis/market/dto/DailyPriceDto.java
@@ -7,16 +7,17 @@ import lombok.Builder;
 public record DailyPriceDto(
         @JsonProperty("rt_cd") String rtCd, // 성공 실패 여부
         @JsonProperty("msg1") String message, // 응답메세지
-        @JsonProperty("output") Output output){ // 응답 상세
+        @JsonProperty("output") DailyPriceRes output){ // 응답 상세
     @Builder
-    public record Output(
+    public record DailyPriceRes(
             @JsonProperty("stck_prpr") String currentPrice,
             @JsonProperty("stck_oprc") String openPrice,
             @JsonProperty("stck_hgpr") String highPrice,
             @JsonProperty("stck_lwpr") String lowPrice,
             @JsonProperty("acml_vol") String volume,
             @JsonProperty("prdy_vrss") String compareYesterday,
-            @JsonProperty("prdy_vrss_sign") String compareYesterdaySign
+            @JsonProperty("prdy_vrss_sign") String compareYesterdaySign,
+            @JsonProperty("prdy_ctrt") String dayChange
     ){}
 }
 
