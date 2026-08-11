8fba1a01d5ef8d4dfe2727d497a1779d8d93f90a
kkangmen <pickofee@gmail.com>
2026-08-11 17:31:19 +0900

    chore: 네이버 뉴스 호출 API 스웨거 설명 추가

--- src/main/java/juby/invest/domain/news/controller/NewsController.java
+++ src/main/java/juby/invest/domain/news/controller/NewsController.java
@@ -1,5 +1,7 @@
 package juby.invest.domain.news.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import juby.invest.domain.news.dto.NewsResDto;
 import juby.invest.domain.news.exception.code.NewsSuccessCode;
 import juby.invest.domain.news.service.NewsService;
@@ -16,11 +18,13 @@ import org.springframework.web.bind.annotation.*;
 @Slf4j
 @RequiredArgsConstructor
 @RequestMapping("/api/news")
+@Tag(name = "네이버 뉴스 호출 API (인증 API)", description = "네이버 뉴스 검색")
 public class NewsController {
 
     private final NewsService newsService;
 
     @GetMapping
+    @Operation(summary = "검색어 기반 네이버 뉴스 검색 API", description = "검색어를 입력하면 20개의 뉴스 데이터가 반환된다.")
     public ApiResponse<NewsResDto.NewsResponse> searchNews(
             @RequestParam("query") String query,
             @AuthenticationPrincipal CustomOAuth2User customOAuth2User
