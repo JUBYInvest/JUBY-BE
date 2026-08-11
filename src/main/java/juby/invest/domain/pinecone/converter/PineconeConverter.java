48aeee542c19b3e2178fc06cf961e3e268aeb4e8
kkangmen <pickofee@gmail.com>
2026-08-11 17:32:49 +0900

    refactor: pinecone에 originallink 열 추가 생성

--- src/main/java/juby/invest/domain/pinecone/converter/PineconeConverter.java
+++ src/main/java/juby/invest/domain/pinecone/converter/PineconeConverter.java
@@ -10,6 +10,8 @@ public class PineconeConverter {
 
     /***
      * 함수 기능: 네이버 뉴스 API 응답을 vectorDB에 넣기 위해 변환한다.
+     *          originallink는 종목 뉴스 조회 API에서 언론사명을 추출하는데 사용한다.
+     *          혹여 비어있을 때를 대비해, null 대신 빈 문자열로 대체한다.
      * @param newsResponse 네이버 뉴스 API 응답
      * @return vectorDB에 넣을 레코드 리스트
      */
@@ -26,10 +28,10 @@ public class PineconeConverter {
             record.put("pubDate", item.pubDate());
             record.put("text", item.title() + item.description());
             record.put("stock_name", stockName);
+            record.put("originallink", Objects.toString(item.originallink(), null));
 
             upsertRecords.add(record);
         }
-
         return upsertRecords;
     }
 }
